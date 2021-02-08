fe890ebe75acdbba328dc45b684ab8f99d1b0867
EmilioDelCastillo <emilio.del-castillo@outlook.com>
2021-02-08 23:40:15 +0100

    Add documentation

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,11 +12,16 @@ class ViewController: UIViewController {
     
     @IBOutlet private var buttons: [UIButton]!
     @IBOutlet private weak var display: UILabel!
+    /**
+        Tells if the current value is decimal or not.
+     */
     private var decimal = false
+    
     @IBAction private func touchButton (_ sender: UIButton){
         
         let numeric = Int(sender.currentTitle!)
         if numeric != nil {
+            // Do not count the comma as a number
             if display.text?.count == 9 + decimal.int {
                 return;
             }
@@ -28,13 +33,16 @@ class ViewController: UIViewController {
             
         } else {
             switch sender.currentTitle {
-            case "AC":
+            case "AC": // Clear everything
                 display.text = "0"
                 decimal = false
-            case "C":
+                // TODO: Clear any operation button
+            
+            case "C": // Clear the display, keep the operations
                 display.text = "0"
                 decimal = false
                 sender.setTitle("AC", for: .normal)
+                
             case ",":
                 if !decimal {
                     display.text?.append(",")
@@ -47,37 +55,53 @@ class ViewController: UIViewController {
                     display.text?.insert("-", at: display.text!.startIndex)
                 }
             case "%":
+                // Without this it would display 0,0
                 if replaceZero() {
                     break
                 }
                 let result = (Double(toComputer(str: display.text!))! / 100)
                 display.text = toHuman(str: String(result))
             default:
+                // This is just a placeholder
                 display.text = "H"
             }
         }
         
     }
     
+    /**
+        Returns true if the initial 0 needs to be replaced.
+     */
     private func replaceZero() -> Bool{
         return display.text?.last == "0"
     }
-    
+    /**
+        Sets the title for the "clear" button to "C".
+     */
     private func setClearButton() {
         if let index = buttons.firstIndex(where: {$0.currentTitle == "AC"}) {
             buttons[index].setTitle("C", for: .normal)
         }
     }
-    
+    /**
+     Returns a new string replacing all the commas with dots.
+     - Parameter str: The source string.
+     */
     private func toComputer(str: String) -> String {
         return str.replacingOccurrences(of: ",", with: ".")
     }
+    
+    /**
+     Returns a new string replacing all the dots with commas.
+     - Parameter str: The source string.
+     */
     private func toHuman(str: String) -> String {
         return str.replacingOccurrences(of: ".", with: ",")
     }
     
     override func viewDidLayoutSubviews() {
         for button in buttons {
+            // This makes the buttons round.
             button.layer.cornerRadius = button.bounds.size.height * 0.5
         }
     }
@@ -85,7 +109,11 @@ class ViewController: UIViewController {
 
 }
 
+
 extension Bool {
+    /**
+     Returns an int corresponding to the state of the boolean, 1 or 0
+     */
     var int: Int {
         return (self) ? 1 : 0
     }
